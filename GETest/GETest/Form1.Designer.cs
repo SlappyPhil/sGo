47ac366ebadeac18c43528e31aa4fe55decae41f
SlappyPhil <slappyphil@gmail.com>
2013-09-30 01:18:20 -0400

    Made a WinForm sample
    This is a WinForm sample that has a Gecko(Mozilla) Browser with the GE
    plugin, and to the left is a text box for recognized gestures and above
    is a placement to add the Kinect Toolkit viewer. You will need to
    install the Gecko reference. I'm still trying to find a way to get these
    browser apps to work in wpf... once I do, street view will be a breeze.

---
+++ GETest/GETest/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace GETest
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.geckoWebBrowser1 = new Skybound.Gecko.GeckoWebBrowser();
+            this.tb_Gestures = new System.Windows.Forms.TextBox();
+            this.lbl_Gestures = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // geckoWebBrowser1
+            // 
+            this.geckoWebBrowser1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.geckoWebBrowser1.Location = new System.Drawing.Point(184, 0);
+            this.geckoWebBrowser1.Name = "geckoWebBrowser1";
+            this.geckoWebBrowser1.Size = new System.Drawing.Size(700, 502);
+            this.geckoWebBrowser1.TabIndex = 0;
+            // 
+            // tb_Gestures
+            // 
+            this.tb_Gestures.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tb_Gestures.Location = new System.Drawing.Point(0, 295);
+            this.tb_Gestures.Multiline = true;
+            this.tb_Gestures.Name = "tb_Gestures";
+            this.tb_Gestures.Size = new System.Drawing.Size(184, 207);
+            this.tb_Gestures.TabIndex = 2;
+            // 
+            // lbl_Gestures
+            // 
+            this.lbl_Gestures.AutoSize = true;
+            this.lbl_Gestures.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lbl_Gestures.Location = new System.Drawing.Point(0, 282);
+            this.lbl_Gestures.Name = "lbl_Gestures";
+            this.lbl_Gestures.Padding = new System.Windows.Forms.Padding(0, 0, 104, 0);
+            this.lbl_Gestures.Size = new System.Drawing.Size(184, 13);
+            this.lbl_Gestures.TabIndex = 3;
+            this.lbl_Gestures.Text = "Gestures Used:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 502);
+            this.Controls.Add(this.lbl_Gestures);
+            this.Controls.Add(this.tb_Gestures);
+            this.Controls.Add(this.geckoWebBrowser1);
+            this.IsMdiContainer = true;
+            this.Name = "MainForm";
+            this.Text = "sGo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Skybound.Gecko.GeckoWebBrowser geckoWebBrowser1;
+        private System.Windows.Forms.TextBox tb_Gestures;
+        private System.Windows.Forms.Label lbl_Gestures;
+    }
+}
+
---
+++ GETest/GETest/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GETest
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            Skybound.Gecko.Xpcom.Initialize(@"D:\xulrunner-1.9.1.1.en-US.win32\xulrunner\");
+            geckoWebBrowser1.BackColor = Color.White;
+            geckoWebBrowser1.NoDefaultContextMenu = true;
+            geckoWebBrowser1.HandleCreated += new EventHandler(geckoBrowser1_HandleCreated);
+         
+        }
+
+        //Handle Created EventHandler
+        private void geckoBrowser1_HandleCreated(object sender, EventArgs e)
+        {
+            geckoWebBrowser1.Navigate(@"http://earth-api-samples.googlecode.com/svn/trunk/demos/desktop-embedded/pluginhost.html");
+        }
+    }
+}
